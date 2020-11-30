7a614ceb445dadb764a6772504c50d9f70d35c01
Sanu <sanu@o2onelabs.com>
2020-12-01 02:03:17 +0530

    initial setup

---
+++ src/KnetServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace O2one\Knet;
+
+use Illuminate\Support\ServiceProvider;
+
+class KnetServiceProvider extends ServiceProvider
+{
+    /**
+     * Publishes configuration file.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../config/knet.php' => config_path('knet.php'),
+            ], 'knet-config');
+        }
+    }
+
+    /**
+     * Make config publishment optional by merge the config from the package.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/knet.php',
+            'knet'
+        );
+    }
+}
